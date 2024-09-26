59932ef7890dd1b0299691ae8ee1531848709803
luperr <lachlannwhitehill@gmail.com>
2024-09-27 00:24:37 +1000

    update setup.sh'

--- setup.sh
+++ setup.sh
@@ -28,6 +28,11 @@ git config --global user.email lachlannwhitehill@gmail.com
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 
 
+echo |
+if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
+    GIT_PROMPT_ONLY_IN_REPO=1
+    source "$HOME/.bash-git-prompt/gitprompt.sh"
+fi >> ~/.bashrc
 
 # Set local user into docker group
 sudo usermod -a -G docker $USER
