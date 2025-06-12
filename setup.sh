b513d81b4f0157a1e53deb313761b8d20e5444a1
luperr <lachlannwhitehill@gmail.com>
2025-06-13 00:35:13 +1000

    adding comments and notes

--- setup.sh
+++ setup.sh
@@ -5,34 +5,25 @@
 DOTFILES_DIR=$HOME/dotfiles
 PACKAGES_DIR=$DOTFILES_DIR/packages
 
-# Install updates and default apt packages
 
+#TODO: paraterise and use system agnostic package manager..... like homebrew, apt or yum (TBH still think NIX would be a good default packagemanager without using NixOS)
+# Install updates and default apt packages
 PACKAGES=$(cat $PACKAGES_DIR/apt.txt | sort -u)
 sudo apt-get -q update && sudo apt-get -q upgrade -y
 echo $PACKAGES | xargs sudo apt-get -q install -y
 
-# Install AWS CLI
-TEMPDIR=$(mktemp -d)
-pushd $TEMPDIR
-curl -s "https://awscli.amazonaws.com/awscli-exe-linux-$ARCH.zip" -o "awscliv2.zip"
-unzip -qq -o awscliv2.zip
-sudo ./aws/install -u
-popd
-
 # Setup Git global config....
 git config --global user.name luperr
 git config --global user.email lachlannwhitehill@gmail.com
-
+#TODO: more useful default git stuff. Like the flobal merger resolve setting. 
 
 # Get Repo for bashrc plugin..... Move to Bashrc file? 
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 
-
-echo |
-if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
-    GIT_PROMPT_ONLY_IN_REPO=1
-    source "$HOME/.bash-git-prompt/gitprompt.sh"
-fi >> ~/.bashrc
+#TODO: append the .bashrc file
+#create the .tmux.conf 
+#append the .bashrc file
+#VIM goodies
 
 # Set local user into docker group
 sudo usermod -a -G docker $USER
